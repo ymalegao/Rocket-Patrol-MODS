287bb469075ba83794277261a6d86defdcd2ff99
Yash <ymalegao@ucsc.edu>
2023-10-13 00:43:22 -0700

    done witg game

---
+++ src/Menu.js
@@ -0,0 +1,59 @@
+class Menu extends Phaser.Scene {
+    constructor(){
+        super("menuScene");
+    }
+    
+    
+    
+    
+    
+    preload(){
+        this.load.audio('sfx_select', './Assets/blip_select12.wav');
+        this.load.audio('sfx_explosion', './Assets/explosion38.wav');
+        this.load.audio('sfx_rocket', './Assets/rocket_shot.wav');
+    }
+    
+    
+    create(){
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+              top: 5,
+              bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        this.add.text(game.config.width/2, game.config.height/2-borderUISize-borderPadding, "ROCKET PATROL", menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, "Use <--> arrows to move and & F to fire", menuConfig).setOrigin(0.5);
+        menuConfig.backgroundColor = "#00FF00";
+        menuConfig.color  = "#000";
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, "Pres <- for Novice or -> for Expert", menuConfig).setOrigin(0.5);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    update(){
+        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            game.settings = {
+            spaceshipSpeed: 3,
+            gameTimer: 60000    
+            }
+            this.sound.play('sfx_select');
+            this.scene.start('playScene');    
+          }
+          if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+            game.settings = {
+            spaceshipSpeed: 4,
+            gameTimer: 45000    
+            }
+            this.sound.play('sfx_select');
+            this.scene.start('playScene');    
+          }
+        }
+}
+
+
